3d2855cfaaba55786e7bcf47f4826a56c7dcac88
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-11 15:41:11 -0700

    Display three messages

---
+++ Chapter_01/Problem$01/Problem$01.java
@@ -0,0 +1,19 @@
+package chapter_one;
+
+/**
+ * 1.1 (Display three messages) Write a program that displays Welcome to Java,
+ * Learning Java Now, and Programming is fun.
+ *
+ * @author Sharaf Qeshta
+ * */
+
+
+public class Problem$01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is Fun");
+    }
+}
