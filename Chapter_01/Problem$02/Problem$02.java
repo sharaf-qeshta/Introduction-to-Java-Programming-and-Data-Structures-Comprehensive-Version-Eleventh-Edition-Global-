662b6df87ca98b64d2b7e5bbacf286ecb7db3aa4
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-11 15:44:16 -0700

    Display five messages

---
+++ Chapter_01/Problem$02/Problem$02.java
@@ -0,0 +1,18 @@
+package chapter_one;
+
+/**
+ * 1.2 (Display five messages) Write a program that displays I love Java five times
+ *
+ * @author Sharaf Qeshta */
+
+public class Problem$02
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I lOVE YOU");
+        System.out.println("I lOVE YOU");
+        System.out.println("I lOVE YOU");
+        System.out.println("I lOVE YOU");
+        System.out.println("I lOVE YOU");
+    }
+}
