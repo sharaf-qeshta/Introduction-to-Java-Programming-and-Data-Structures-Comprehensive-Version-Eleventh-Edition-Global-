855a3d831c12a80fe849a040393a736bc478fb1d
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-12 11:10:59 -0700

    Display a pattern

---
+++ Chapter_01/Problem$03/Problem$03.java
@@ -0,0 +1,22 @@
+package chapter_one;
+
+/**
+ * *1.3 (Display a pattern) Write a program that displays the following pattern:
+ *  J
+ * J aaa v vaaa
+ * J J aa v v a a
+ * J aaaa v aaaa
+ *
+ * @author Sharaf Qeshta
+ */
+
+public class Problem$03
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   j       ");
+        System.out.println("J aaa    v    vaaa");
+        System.out.println("J J   aa   v v   a a");
+        System.out.println("J   aaaa     v   aaaa");
+    }
+}
