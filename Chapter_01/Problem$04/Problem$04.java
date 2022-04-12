f1171c2cff08216f9fb72eaf02db0cc0b93a303e
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-12 11:14:41 -0700

    Print a table

---
+++ Chapter_01/Problem$04/Problem$04.java
@@ -0,0 +1,24 @@
+package chapter_one;
+
+/**
+ * 1.4 (Print a table) Write a program that displays the following table:
+ * a a^2 a^3 a^4
+ * 1  1   1   1
+ * 2  4   8   16
+ * 3  9  27   81
+ * 4  16 64   256
+ *
+ * @author Sharaf Qeshta
+ * */
+
+public class Problem$04
+{
+    public static void main(String[] args)
+    {
+        System.out.println("a  a^2  a^3  a^4");
+        System.out.println("1  1    1    1");
+        System.out.println("2  4    8    16");
+        System.out.println("3  9    27   81");
+        System.out.println("4  16   64   256");
+    }
+}
