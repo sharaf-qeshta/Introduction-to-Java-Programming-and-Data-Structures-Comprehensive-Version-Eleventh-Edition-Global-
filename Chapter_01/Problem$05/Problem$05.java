7ae020bb15b99e0faeb1d1d3134d29d9438263f3
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-17 12:59:19 -0700

    Compute expressions

---
+++ Chapter_01/Problem$05/Problem$05.java
@@ -0,0 +1,14 @@
+package chapter_one;
+
+/**
+ * 1.5 (Compute expressions) Write a program that displays the result of
+ *      7.5 * 6.5 - 4.5 * 3 / 47.5 - 5.5
+ * */
+
+public class Problem$05
+{
+    public static void main(String[] args)
+    {
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
+    }
+}
