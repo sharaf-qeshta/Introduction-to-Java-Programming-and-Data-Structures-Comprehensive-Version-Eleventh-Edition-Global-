1449603b315cc5f0d6300f5402c860e0819e8a4f
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-17 13:01:11 -0700

    Summation of a series

---
+++ Chapter_01/Problem$06/Problem$06.java
@@ -0,0 +1,14 @@
+package chapter_one;
+
+/**
+ * 1.6 (Summation of a series) Write a program that displays the result of
+ *          1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10.
+ * */
+
+public class Problem$06
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+    }
+}
