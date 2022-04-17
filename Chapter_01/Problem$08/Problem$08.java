1afd147d0472ab136c5b26c500c0f864e77f122e
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-04-17 13:08:33 -0700

    Area and perimeter of a circle

---
+++ Chapter_01/Problem$08/Problem$08.java
@@ -0,0 +1,20 @@
+package chapter_one;
+
+/**
+ * 1.8 (Area and perimeter of a circle) Write a program that displays the area
+ * and perimeter of a circle that has a radius of 6.5 using the following formula:
+ * π = 3.14159
+ * perimeter = 2 * radius * π
+ * area = radius * radius * π
+ *
+ * @author Sharaf Qeshta
+ * */
+
+public class Problem$08
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2 * 6.5 *  3.14159);
+        System.out.println(6.5 * 6.5 *  3.14159);
+    }
+}
