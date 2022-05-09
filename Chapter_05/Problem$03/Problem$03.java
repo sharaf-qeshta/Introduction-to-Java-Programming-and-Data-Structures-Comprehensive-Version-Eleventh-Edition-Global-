cf2d030c1351bd347ee110fdd6b1baedc3a9c5cd
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-05-09 09:41:08 -0700

    Conversion from Cº to Fº

---
+++ Chapter_05/Problem$03/Problem$03.java
@@ -0,0 +1,26 @@
+package chapter_five;
+
+/**
+ * 5.3 (Conversion from Cº to Fº) Write a program that displays the following table (note
+ * that farenheit = celsius * 9/5 + 32):
+ * Celsius  Fahrenheit
+ * 0        32.0
+ * 2        35.6
+ *    ...
+ * 98       208.4
+ * 100      212.0
+ *
+ * */
+
+public class Problem$03
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Celsius" + "\t\tFahrenheit");
+        for (int i = 0; i < 101; i += 2)
+        {
+            double fahrenheit = i * 9.0 / 5 + 32;
+            System.out.println(i + "\t\t\t\t" + fahrenheit);
+        }
+    }
+}
