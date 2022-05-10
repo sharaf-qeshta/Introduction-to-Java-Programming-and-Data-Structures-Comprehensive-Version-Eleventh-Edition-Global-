f9c1be9e000253b25bdec24210c27c4d154195f7
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-05-10 08:50:25 -0700

    Find the smallest n such that n3 > 12,000

--- Chapter_05/Problem$12/Problem$12.java
+++ Chapter_05/Problem$12/Problem$12.java
@@ -1,8 +1,8 @@
 package chapter_five;
 
 /**
- * 5.12 (Find the smallest n such that n3 > 12,000) Use a while loop to find the smallest
- * integer n such that n3 is greater than 12,000.
+ * 5.12 (Find the smallest n such that n3 7 12,000) Use a while loop to find the smallest
+ * integer n such that n3 is greater than 12,000
  *
  * @author Sharaf Qeshta
  * */
@@ -12,12 +12,8 @@ public class Problem$12
     public static void main(String[] args)
     {
         int i = 0;
-        int iCubed = 0;
-        while (iCubed < 12000)
-        {
+        while ( i * i * i < 12000)
             i++;
-            iCubed = i * i * i;
-        }
         System.out.println(i + " is the desired number! :]");  // 23
     }
 }
