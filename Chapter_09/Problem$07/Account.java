2871cfbf64d25472a70baf80e425dc0923232ee0
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-05-27 16:08:00 -0700

    The Account class

---
+++ Chapter_09/Problem$07/Account.java
@@ -0,0 +1,73 @@
+package chapter_nine;
+
+import java.util.Date;
+
+public class Account
+{
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private final Date DATE_CREATED = new Date();
+
+    public Account() { }
+
+
+    public Account(int id, double balance)
+    {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate()
+    {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate)
+    {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDATE_CREATED()
+    {
+        return DATE_CREATED;
+    }
+
+    private double getMonthlyInterestRate(){
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest(){
+        return getMonthlyInterestRate() * balance;
+    }
+
+    public void withdraw(double amount)
+    {
+        balance -= amount;
+    }
+
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+}
