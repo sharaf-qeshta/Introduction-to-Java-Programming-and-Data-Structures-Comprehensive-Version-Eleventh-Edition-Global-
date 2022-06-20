fd1656282f971e8ab1405a12cc2c9fd00d446a6b
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-06-20 06:08:25 -0700

    Use the ClockPane class

--- Chapter_14/Problem$26/Problem$26.java
+++ Chapter_14/Problem$26/Problem$26.java
@@ -1,7 +1,7 @@
 package chapter_fourteen;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.Pos; 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
