d7ad6e3a0c7e1f16618f3561230bbb051d687b6a
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-08-26 09:40:58 -0700

    PrimeIterator

---
+++ Chapter_24/Problem$14/PrimeIterator.java
@@ -0,0 +1,41 @@
+package chapter_twenty_four;
+
+import java.util.Iterator;
+
+public class PrimeIterator implements Iterator<Integer>
+{
+    int current = 2;
+    int limit;
+
+    public PrimeIterator(int limit)
+    {
+       this.limit = limit;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+        return current <= limit;
+    }
+
+    @Override
+    public Integer next()
+    {
+        int next = current+1;
+        boolean found = false;
+        while (!found)
+        {
+            found = true;
+            for (int i = 2; i < next; i++)
+                if (next % i == 0)
+                {
+                    found = false;
+                    next++;
+                    break;
+                }
+        }
+        int out = current;
+        current = next;
+        return out;
+    }
+}
---
+++ Chapter_24/Problem$14/Problem$14.java
@@ -0,0 +1,24 @@
+package chapter_twenty_four;
+
+/**
+ * *24.14 (Prime number iterator) Define an iterator class named PrimeIterator for
+ * iterating prime numbers. The constructor takes an argument that specifies the limit
+ * of the maximum prime number. For example, new PrimeIterator(23302)
+ * creates an iterator that iterates prime numbers less than or equal to 23302. Write
+ * a test program that uses this iterator to display all prime numbers less than or
+ * equal to 120000.
+ *
+ *
+ * @author Sharaf Qeshta
+ * */
+public class Problem$14
+{
+    public static void main(String[] args)
+    {
+        PrimeIterator iterator = new PrimeIterator(12_000);
+
+        /* 2 3 5 7 11 13 17 19 23 29 31 37 41 ........ 11953 11959 11969 11971 11981 11987 */
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+    }
+}
