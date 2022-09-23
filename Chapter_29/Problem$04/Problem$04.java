a897915dd91cc2d4b645394e366505d4f9a0b028
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-09-23 09:52:58 -0700

    Modify weight in the nine tails problem

---
+++ Chapter_29/Problem$04/Problem$04.java
@@ -0,0 +1,14 @@
+package chapter_twenty_nine;
+
+/**
+ * *29.4 (Modify weight in the nine tails problem) In the text, we assign the number of
+ * the flips as the weight for each move. Assuming the weight is three times of the
+ * number of flips, revise the program.
+ *
+ *
+ * @author Sharaf Qeshta
+ * */
+public class Problem$04
+{
+    // see WeightedNineTailModel.java
+}
