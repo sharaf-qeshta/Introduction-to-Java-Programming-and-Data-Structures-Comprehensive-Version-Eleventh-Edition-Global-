4484813b42b48a4680a0f9df32032fec17d59461
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-09-29 16:37:08 -0700

    Dynamic graphs

---
+++ Chapter_29/Problem$15/Displayable.java
@@ -0,0 +1,11 @@
+package chapter_twenty_nine;
+
+
+public interface Displayable
+{
+    public double getX(); // Get x-coordinate of the vertex
+
+    public double getY(); // Get y-coordinate of the vertex
+
+    public String getName(); // Get display name of the vertex
+}
