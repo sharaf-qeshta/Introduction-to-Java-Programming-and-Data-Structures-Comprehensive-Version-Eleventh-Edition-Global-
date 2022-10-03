ee85d0bda2fc5a10070629251c9634985a7022f8
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-10-02 22:34:51 -0700

    Weighted Graphs and Applications

---
+++ Chapter_29/samples/WeightedEdge.java
@@ -0,0 +1,33 @@
+package chapter_twenty_nine.samples;
+
+/**
+ * Listing 29.1 WeightedEdge.java
+ * */
+public class WeightedEdge extends Edge
+        implements Comparable<WeightedEdge>
+{
+    public double weight; // The weight on edge (u, v)
+
+    /**
+     * Create a weighted edge on (u, v)
+     */
+    public WeightedEdge(int u, int v, double weight)
+    {
+        super(u, v);
+        this.weight = weight;
+    }
+
+    /**
+     * Compare two edges on weights
+     */
+    @Override
+    public int compareTo(WeightedEdge edge)
+    {
+        if (weight > edge.weight)
+            return 1;
+        else if (weight == edge.weight)
+            return 0;
+        else
+            return -1;
+    }
+}
