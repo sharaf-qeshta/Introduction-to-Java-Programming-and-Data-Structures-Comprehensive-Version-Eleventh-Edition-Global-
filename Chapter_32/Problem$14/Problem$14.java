90efbeeb26109c3c6183de6502ec575b25db714d
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-10-17 05:50:08 -0700

    Parallel quick sort

--- Chapter_32/Problem$14/Problem$14.java
+++ Chapter_32/Problem$14/Problem$14.java
@@ -1,6 +1,5 @@
 package chapter_thirty_two;
 
-import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
@@ -20,13 +19,23 @@ public class Problem$14
 {
     public static void main(String[] args)
     {
-        int[] list = {-1, 54, 14, 5, 7, 2, 21, -3};
-        parallelQuickSort(list);
+        int[] list = new int[9_000_000];
+        for (int i = 0; i < 9_000_000; i++)
+            list[i] = (int) (Math.random() * 100);
 
-        /*
-        * [-3, -1, 2, 5, 7, 14, 21, 54]
-        * */
-        System.out.println(Arrays.toString(list));
+        long start = System.currentTimeMillis();
+        parallelQuickSort(list);
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken for parallel quick sort: " + (end - start));
+
+        int[] list2 = new int[9_000_000];
+        for (int i = 0; i < 9_000_000; i++)
+            list2[i] = (int) (Math.random() * 100);
+
+        start = System.currentTimeMillis();
+        sequentialQuickSort(list2);
+        end = System.currentTimeMillis();
+        System.out.println("Time taken for sequential quick sort: " + (end - start));
     }
 
     public static void parallelQuickSort(int[] list)
@@ -34,6 +43,21 @@ public class Problem$14
         RecursiveAction mainTask = new SortTask(list, 0, list.length-1);
         ForkJoinPool pool = new ForkJoinPool();
         pool.invoke(mainTask);
+    }
+
+    public static void sequentialQuickSort(int[] list)
+    {
+        sequentialQuickSort(list, 0, list.length-1);
+    }
+
+    public static void sequentialQuickSort(int[] list, int first, int last)
+    {
+        if (last > first)
+        {
+            int pivot = partition(list, first, last);
+            sequentialQuickSort(list, first, pivot-1);
+            sequentialQuickSort(list, pivot+1, last);
+        }
     }
 
     private static class SortTask extends RecursiveAction
@@ -48,7 +72,6 @@ public class Problem$14
             this.last = last;
         }
 
-
         @Override
         protected void compute()
         {
@@ -59,42 +82,42 @@ public class Problem$14
                         new SortTask(list, pivotIndex+1, last));
             }
         }
+    }
 
-        public static int partition(int[] list, int first, int last)
+    public static int partition(int[] list, int first, int last)
+    {
+        int pivot = list[first];
+        int low = first + 1;
+        int high = last;
+
+        while (high > low)
         {
-            int pivot = list[first];
-            int low = first + 1;
-            int high = last;
+            while (low <= high && list[low] <= pivot)
+                low++;
 
-            while (high > low)
-            {
-                while (low <= high && list[low] <= pivot)
-                    low++;
-
-                while (low <= high && list[high] > pivot)
-                    high--;
-
-                if (high > low)
-                {
-                    int temp = list[high];
-                    list[high] = list[low];
-                    list[low] = temp;
-                }
-            }
-
-            while (high > first && list[high] >= pivot)
+            while (low <= high && list[high] > pivot)
                 high--;
 
-            if (pivot > list[high])
+            if (high > low)
             {
-                list[first] = list[high];
-                list[high] = pivot;
-                return high;
+                int temp = list[high];
+                list[high] = list[low];
+                list[low] = temp;
             }
-            else
-            {
-                return first;
-            }
+        }
+
+        while (high > first && list[high] >= pivot)
+            high--;
+
+        if (pivot > list[high])
+        {
+            list[first] = list[high];
+            list[high] = pivot;
+            return high;
+        }
+        else
+        {
+            return first;
         }
     }
 }
