e7f4a1a8598c57dbc9ff24fd50f3ec4264fa1043
sharaf-qeshta <sharaf.keshta@gmail.com>
2022-11-01 22:02:28 -0700

    Java Database Programming

---
+++ Chapter_34/samples/TestResultSetMetaData.java
@@ -0,0 +1,42 @@
+package chapter_thirty_four.samples;
+
+import java.sql.*;
+
+public class TestResultSetMetaData
+{
+    public static void main(String[] args)
+            throws SQLException, ClassNotFoundException
+    {
+        // Load the JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Driver loaded");
+
+        // Connect to a database
+        Connection connection = DriverManager.getConnection
+                ("jdbc:mysql://localhost/javabook", "scott", "tiger");
+        System.out.println("Database connected");
+
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Execute a statement
+        ResultSet resultSet = statement.executeQuery
+                ("select * from Enrollment");
+
+        ResultSetMetaData rsMetaData = resultSet.getMetaData();
+        for (int i = 1; i <= rsMetaData.getColumnCount(); i++)
+            System.out.printf("%-12s\t", rsMetaData.getColumnName(i));
+        System.out.println();
+
+        // Iterate through the result and print the students' names
+        while (resultSet.next())
+        {
+            for (int i = 1; i <= rsMetaData.getColumnCount(); i++)
+                System.out.printf("%-12s\t", resultSet.getObject(i));
+            System.out.println();
+        }
+
+        // Close the connection
+        connection.close();
+    }
+}
